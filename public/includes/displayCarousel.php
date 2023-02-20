7aa82e03222c5f4735f078ccd16224ec0bc3c03b
mathias <mathias.lepoittevin@gmail.com>
2023-02-20 23:50:28 +0100

    [END]Last commit

--- public/includes/displayCarousel.php
+++ public/includes/displayCarousel.php
@@ -2,7 +2,7 @@
     <div class="d-flex justify-content-center">
         <img class="img-fluid d-block" src="public/src/img/uploads/<?=$file?>" alt="Premier slide">
         <div class="carousel-caption hover">
-            <h5><?=explode('.',$file)[0]?>></h5>
+            <h5><?=explode('.',$file)[0]?></h5>
         </div>
     </div>
 </div>

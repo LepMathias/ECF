ef088fb71d859aeef02308a046662944853fd717
mathias <mathias.lepoittevin@gmail.com>
2023-04-04 14:24:09 +0200

    [UPD]confdb.php

--- db/confDB.php
+++ db/confDB.php
@@ -1,22 +1,21 @@
 <?php
+if(!isset($_ENV['JAWSDB_URL'])){
+    /**
+     * Conf to run on local
+     */
+    $HOST = 'localhost';
+    $DB = 'restaurant';
+    $USER = 'mathias';
+    $PWD = 'E.300619';
+} else {
+    /**
+     * Conf to run via heroku
+     */
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
 
-/**
- * Conf to run on local
- */
-
-$HOST = '127.0.0.1';
-$DB = 'restaurant';
-$USER = 'root';
-$PWD = '';
-
-
-/*Conf to run via heroku*/
-/*
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
-$HOST = $dbparts['host'];
-$USER = $dbparts['user'];
-$PWD = $dbparts['pass'];
-$DB = ltrim($dbparts['path'],'/');
-*/
+    $HOST = $dbparts['host'];
+    $USER = $dbparts['user'];
+    $PWD = $dbparts['pass'];
+    $DB = ltrim($dbparts['path'], '/');
+}
